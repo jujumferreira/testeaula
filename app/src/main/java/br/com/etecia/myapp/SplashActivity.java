8491e31bc970619740a8b1f833e2cc5899426d79
jujumferreira <juliadasilvanferreira@gmail.com>
2024-05-02 20:19:41 -0300

    teste aula

---
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -0,0 +1,36 @@
+package br.com.etecia.myapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.splash_layout);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Criar método thread para abrir janela
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Abrir janela de login
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+
+            }
+        },2000);
+    }
+}
